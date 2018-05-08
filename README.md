66e174f4b109b2621abf74ee9196b433afb76ef5
Ryan Loong <rmloong@gmail.com>
2018-05-08 14:56:28 -0700

    scraper 2016 halfway finished but crashed.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # movie-revenue-predictor
 
+#May 8, 2018
+
 The entertainment business is inherently an industry with quite a bit of risk. Each individual movie has budgets, essentially investments, that can exceed hundreds of millions of dollars. It is not a guarantee whether these investments will pay off. It would be beneficial for studios to be able to predict how much a movie will make in gross receipts based upon a variety of combination of obvious and non-obvious factors. They can then make better decisions on which projects to greenlight, and even perhaps limit (or scale up) the appropriate budget.
 
 This model is intended to assist movie studios in making decisions on whether or not to move ahead with a certain movie. This is usually years before the movie even comes out. Other models might use additional data based upon data that can be gathered leading up to a movie’s release (trailer views, social media data, etc.) but that information would not be available to a studio who is making their decision before any of this happens.
--- src/webscrape.py
+++ src/webscrape.py
@@ -16,19 +16,30 @@ def create_mongodb():
 
     #Webscrape and add each movie html data to the collection movies from the movies_metadata db
     browser = webdriver.Firefox()
-    for i in range(2017,2001,-1)
-        for j in range(474,600):
-            browser.get("https://www.the-numbers.com/United-States/movies/year/"+str(i))
-            #If the worldwide revenue is a nonzero amount... then go to the direct webpage for the movie
-            #Then store that webpage's html code in the pymongo collection
-            if (browser.find_element_by_css_selector("#page_filling_chart > center:nth-child(7) > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(" + str(i) + ") > td:nth-child(5)").text != '$0'):
-                url = browser.find_element_by_css_selector("#page_filling_chart > center:nth-child(7) > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(" + str(i) + ") > td:nth-child(1) > b:nth-child(1) > a:nth-child(1)").get_attribute('href')
-                DELAY = 0.2 + np.random.random() # time to wait between HTTP requests
-                time.sleep(DELAY)  # pause between HTTP requests
-                browser.get(url)
-                html = browser.page_source
-                movies.insert_one({'url': url,
-                                 'ts': time.time(),
-                                 'html': html
-                                 })
-                #print(i)
+
+    for i in range(2017,2008,-1):
+        scrape_one_year(i)
+
+    return None
+
+def scrape_one_year(year):
+    browser.get("https://www.the-numbers.com/United-States/movies/year/" + str(year))
+    i = 2
+    while (browser.find_element_by_css_selector("#page_filling_chart > center:nth-child(7) > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(" + str(i) + ") > td:nth-child(5)").text != '$0'):
+        #If the worldwide revenue is a nonzero amount... then go to the direct webpage for the movie
+        #Then store that webpage's html code in the pymongo collection
+        url = browser.find_element_by_css_selector("#page_filling_chart > center:nth-child(7) > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(" + str(i) + ") > td:nth-child(1) > b:nth-child(1) > a:nth-child(1)").get_attribute('href')
+        DELAY = 0.1 + 0.5 * np.random.random() # time to wait between HTTP requests
+        time.sleep(DELAY)  # pause between HTTP requests
+        browser.get(url)
+        html = browser.page_source
+        movies.insert_one({'url': url,
+                         'ts': time.time(),
+                         'html': html
+                         })
+        # print(i)
+        i += 1
+        browser.get("https://www.the-numbers.com/United-States/movies/year/2016")
+    return None
+
+create_mongodb()
