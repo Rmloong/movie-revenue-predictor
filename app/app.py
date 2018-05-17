50b642ce9ea35cb9d93906228f130e9537ce6021
Ryan Loong <rmloong@gmail.com>
2018-05-17 15:38:53 -0700

    played around with some budgeting issue to improve model

--- app/app.py
+++ app/app.py
@@ -10,12 +10,24 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('quadratic.html')
+    return render_template('index.html')
 
 @app.route('/solve', methods=['POST'])
 def solve():
+    '''
+    Grabs the user inputs from json format and converts them to python variables
+    Makes a prediction on those variables
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    json-ified version of the prediction statement
+    '''
     user_data = request.json
-    print(user_data)
+
     budget = user_data['budget']
     franchise = user_data['franchise']
     rating = user_data['rating']
@@ -25,23 +37,47 @@ def solve():
     source = user_data['source']
     month = user_data['month']
 
-    root_1 = _solve_quadratic(budget, franchise, rating, genre)
     single_pred_test_2 = create_prediction_array(budget, franchise, rating, genre, prod_method, creative_type, source, month)
     prediction = np.exp(model.predict(single_pred_test_2))
-    print(model_columns)
-    print(single_pred_test_2)
+    root_1 = print_prediction(prediction)
+    print(user_data)
+    # print(model_columns)
+    # print(single_pred_test_2)
+    # print(prediction)
+    return jsonify({'root_1': root_1})
 
-    return jsonify({'root_1': root_1, 'prediction': list(prediction)})
+def print_prediction(prediction):
+    '''
+    Formats the prediction value into a currency format and converts it to
+    html code format to print to the webpage
 
-def _solve_quadratic(budget, franchise, rating, genre):
-    log_b = np.log(budget)
-    return f'You want to make a prediction of a movie:\
-            <p> in the <b>{genre}</b> Genre,\
-            <p> with an MPAA Rating of <b>{rating}</b>,\
-            <p> with a budget of <b>{budget}</b> and a\
-            <p> log_budget of <b>{log_b}</b>'
+    Parameters
+    ----------
+    prediction in float format
+
+    Returns
+    -------
+    html code to print the prediction
+    '''
+    prediction_str = '${:,.0f}'.format(prediction[0])
+    return f'<p> Your prediction is: ' + prediction_str
 
 def create_prediction_array(budget, franchise, rating, genre, prod_method, creative_type, source, month):
+    '''
+    Creates a prediction array to be called upon to make a prediction.
+    Default array is defined below as df_single_pred (filled with mostly zeros).
+    Converts user input variables to 1's for each input
+
+    Parameters
+    ----------
+    The column titles that the user specified (eg. Genre: Action,
+    Source: Original Screenplay, Month: June)
+
+    Returns
+    -------
+    A 2d pandas df (although it is only a single row) with the correct user defined
+    prediction variables
+    '''
     df_single_pred = pd.DataFrame(np.array([[ 15.,   90.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,
              0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,
              0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,
@@ -70,6 +106,8 @@ def create_prediction_array(budget, franchise, rating, genre, prod_method, creat
 
     df_single_pred.loc[0][month] = 1
 
+    print(df_single_pred)
+    
     return df_single_pred
 
 if __name__ == '__main__':
